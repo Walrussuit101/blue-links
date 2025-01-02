8994697c7f81989e235ca1418240f459b429eaff
Walrussuit101 <tjefferson@ycp.edu>
2025-01-02 15:39:13 -0500

    adjust source in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="footer flex justify-center items-center py-5 bg-zinc-900 text-white">
             <div>
-                <span><a className="underline" href="https://github.com/Walrussuit101/blue-links"  target="_blank" rel="noreferrer">source</a> |  Made by <a className="underline" href="https://timjefferson.info" target="_blank">Tim Jefferson</a></span>
+                <span>Made by <a className="underline" href="https://timjefferson.info" target="_blank">Tim Jefferson</a> | <a className="underline" href="https://github.com/Walrussuit101/blue-links"  target="_blank" rel="noreferrer">Source</a></span>
             </div>
         </div>
     )
